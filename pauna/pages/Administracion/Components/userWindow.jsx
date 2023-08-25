9e617e51aed7287b0750e5a73c66a2841083fc8a
sebasaravi <sebastian.arias.aviles@est.una.ac.cr>
2023-08-25 10:43:36 -0600

    Add a user window

--- pauna/pages/Administracion/Components/userWindow.jsx
+++ pauna/pages/Administracion/Components/userWindow.jsx
@@ -1,13 +1,43 @@
+import { useState } from "react";
 import Sidebar from "./Sidebar";
 
 
 export default function UserWindow() {
+    const [open, setOpen] = useState(false)
     return (
         <>
-            <Sidebar />
-            <main>
-                hello
-            </main>
+            <div className="bg-[#D9D9D9] h-screen">
+                <div className="bg-white text-center text-2xl font-semibold w-max h-max p-4 rounded-lg ">
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className={` text-center absolute ${open && "hidden"}`}>Cedula</label>
+                        <input className="bg-[#989898] rounded-lg" type="text" onClick={()=>setOpen(!open)}></input>
+                    </div>
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className="text-white text-center absolute">Primer Nombre</label>
+                        <input className="bg-[#989898] rounded-lg" type="text"></input>
+                    </div>
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className="text-white text-center absolute">Segundo Nombre</label>
+                        <input className="bg-[#989898] rounded-lg" type="text"></input>
+                    </div>
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className="text-white text-center absolute">Primer Apellido</label>
+                        <input className="bg-[#989898] rounded-lg" type="text"></input>
+                    </div>
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className="text-white text-center absolute">Segundo Apellido</label>
+                        <input className="bg-[#989898] rounded-lg" type="text"></input>
+                    </div>
+                    <div className={`bg-black text-center flex items-center justify-center`}>
+                        <label className="text-white text-center absolute"></label>
+                        <select className="bg-[#989898] rounded-lg" type="">
+                            <option value="manzana">Administrador</option>
+                            <option value="man">Ad</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            
         </>
 
 

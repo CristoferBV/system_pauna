784281f427031ef545f88e7aff534c60d218f609
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-08-26 10:45:46 -0600

    Create new component Register

--- pauna/pages/index.js
+++ pauna/pages/index.js
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google'
 import Sidebar from './Administracion/Components/Sidebar'
 import UserWindow from './Administracion/Components/userWindow'
 import Login from './Login and Register/Login/Login'
+import Register from './Login and Register/Register/Register'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -10,9 +11,9 @@ export default function Home() {
   return (
     <>
       {/* <UserWindow/> */}
-      <UserWindow/>
+      {/* <UserWindow/> */}
       {/* <Login/> */}
-      
+      <Register/>
     </>
   )
 }

8f0f2696becf3e765bc9da0bce3f15a66821b33b
CristoferBV <70454042+CristoferBV@users.noreply.github.com>
2023-08-27 21:51:32 -0600

    Updates

--- pauna/pages/index.js
+++ pauna/pages/index.js
@@ -2,11 +2,6 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Sidebar from './Administracion/Components/Sidebar'
 import UserWindow from './Administracion/Components/userWindow'
-import Login from './Login and Register/Login/Login'
-import Register from './Login and Register/Register/Register'
-import HomeClient from './Biblioteca/Cliente/Components/HomeClient'
-import LoanClient from './Biblioteca/Cliente/Components/LoanClient'
-import DevolutionClient from './Biblioteca/Cliente/Components/DevolutionClient'
 import ProfileClient from './Biblioteca/Cliente/Components/ProfileClient'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -15,13 +10,13 @@ export default function Home() {
   return (
     <>
       {/* <UserWindow/> */}
-      {/* <UserWindow/> */}
+      <UserWindow/>
       {/* <Login/> */}
       {/* <Register/> */}
       {/* <HomeClient/> */}
       {/* <LoanClient/> */}
       {/* <DevolutionClient/> */}
-      <ProfileClient/>
+      {/* <ProfileClient/> */}
       
     </>
   )

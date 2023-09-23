e470a6c244a0b37cd6df06b5f86b65af54f6d024
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-09-22 18:18:05 -0600

    Update DB
    Metodo verificacion de la conexion con la base de datos de prueba

--- pauna/pages/BD/Storage.js
+++ pauna/pages/BD/Storage.js
@@ -5,14 +5,30 @@ const mysql = require('mysql2');
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'root',
-  database: 'pruebadb'
+  database: 'pruebadb',
+  password: ''
 });
 
-// simple query
-connection.query(
-  'SELECT * FROM `usuario` WHERE `nombre` = "Kevin" AND `carrera` = "Administración"',
-  function(err, results, fields) {
-    console.log(results); // results contains rows returned by server
-    console.log(fields); // fields contains extra meta data about results, if available
+connection.connect(function (error) {
+  if (error) {
+    throw error;
+  } else {
+    console.log("Conexion exitosa!");
+
+    // Mantener el programa en ejecución durante 10 segundos
+    setTimeout(function () {
+      connection.end();
+      console.log("Conexión cerrada. Programa terminado.");
+    }, 10000); // 10000 milisegundos = 10 segundos
   }
-);
+});
+
+
+// simple query
+// connection.query(
+//   'SELECT * FROM `login` WHERE `userName` = "Andrey" AND `role` = "Biblioteca"',
+//   function(err, results, fields) {
+//     console.log(results); // results contains rows returned by server
+//     console.log(fields); // fields contains extra meta data about results, if available
+//   }
+// );

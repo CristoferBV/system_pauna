2ec05ba087944ce3ac7222c3d5e24ba755ef2109
sebasaravi <sebastian.arias.aviles@est.una.ac.cr>
2023-09-27 15:52:14 -0600

    Create DBPool

--- pauna/pages/BD/Storage.js
+++ pauna/pages/BD/Storage.js
@@ -1,34 +1,12 @@
-// get the client
-const mysql = require('mysql2');
-
-// create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  database: 'pruebadb',
-  password: ''
-});
-
-connection.connect(function (error) {
-  if (error) {
-    throw error;
-  } else {
-    console.log("Conexion exitosa!");
-
-    // Mantener el programa en ejecución durante 10 segundos
-    setTimeout(function () {
-      connection.end();
-      console.log("Conexión cerrada. Programa terminado.");
-    }, 10000); // 10000 milisegundos = 10 segundos
-  }
-});
+import {createPool} from "mysql2";
 
 
-// simple query
-// connection.query(
-//   'SELECT * FROM `login` WHERE `userName` = "Andrey" AND `role` = "Biblioteca"',
-//   function(err, results, fields) {
-//     console.log(results); // results contains rows returned by server
-//     console.log(fields); // fields contains extra meta data about results, if available
-//   }
-// );
+const pool= createPool({
+  host: "localhost",
+  user:"root",
+  password:"",
+  port:3306,
+  database:"test"
+})
+
+export {pool};

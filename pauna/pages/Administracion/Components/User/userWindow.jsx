1938b66b2bc9dc9039d22d4a2de883b00c36c059
sebasaravi <sebastian.arias.aviles@est.una.ac.cr>
2023-09-27 23:48:52 -0600

    Update method to insert in database

--- pauna/pages/Administracion/Components/User/userWindow.jsx
+++ pauna/pages/Administracion/Components/User/userWindow.jsx
@@ -1,183 +1,202 @@
-'use client'
+"use client";
 import { useState } from "react";
-import axios from 'axios'
-
+import axios from "axios";
 
 export default function UserWindow() {
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log("hola");
+    const res = await axios
+      .post("/api/config/admin", user)
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  const [user, setUser] = useState({
+    UO_identificador: "",
+    UO_primer_nombre: "",
+    UO_segundo_nombre: "",
+    UO_primer_apellido: "",
+    UO_segundo_apellido: "",
+  });
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log("hola")
-        const res = await axios.post('/api/config/admin',{
-            RL_identificador: 'R3',
-            RL_nombre: 'pRUEBA',
-            RL_descripcion: 'DSFASDF'
-        }).then(function (response) {
-            console.log(response);
-          })
-          .catch(function (error) {
-            console.log(error);
-          });
-    };
+  const handleChange = ({ target: { name, value } }) => {
+    setUser({ ...user, [name]: value });
+  };
 
-    const [open, setOpen] = useState(false)
-    return (
-        <>
-            <div className="xl:w-full xl:grid xl:grid-cols-2
+  const [open, setOpen] = useState(false);
+  return (
+    <>
+      <div
+        className="xl:w-full xl:grid xl:grid-cols-2
             lg:w-full lg:grid lg:grid-cols-1 
             p-36 
-            text-3xl font-semibold ">
-                <form  onSubmit={handleSubmit} className="bg-[#D9D9D9] text-center mx-10 rounded
-                lg:mb-4">
-                    <h1 className="text-white p-10">Agrega un administrador</h1>
-                    <div className="p-2
-                    lg:p-0">
-                        <input className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
-                            placeholder="Código">
-                        </input>
-                    </div>
-                    <div className="p-2
-                    lg:p-0">
-                        <input className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
-                            placeholder="Primer Nombre">
-                        </input>
-                    </div>
-                    <div className="p-2
-                    lg:p-0">
-                        <input className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
-                            placeholder="Segundo Nombre*">
-                        </input>
-                    </div>
-                    <div className="p-2
-                    lg:p-0">
-                        <input className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
-                            placeholder="Primer Apellido">
-                        </input>
-                    </div>
-                    <div className="p-2
-                    lg:p-0">
-                        <input className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
-                            placeholder="Segundo Apellido">
-                        </input>
-                    </div>
-                    <div className="p-2
-                    lg:p-0">
-                        <select className="bg-white text-[#D9D9D9] text-center placeholder:text-[#D9D9D9] rounded"
-                            >
-                            <option className=" text-[#D9D9D9] text-center" disabled>Elige una ubicacion</option>
-                            <option className=" text-[#D9D9D9] text-center" value="Administrador">Administrador</option>
-                            <option className="text-[#D9D9D9] text-center" value="Usuario">Usuario</option>
-                        </select>
-                    </div>
+            text-3xl font-semibold "
+      >
+        <form
+          onSubmit={handleSubmit}
+          className="bg-[#D9D9D9] text-center mx-10 rounded
+                lg:mb-4"
+        >
+          <h1 className="text-white p-10">Agrega un administrador</h1>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <input
+              className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
+              placeholder="Código"
+              name="UO_identificador"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <input
+              className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
+              placeholder="Primer Nombre"
+              name="UO_primer_nombre"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <input
+              className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
+              placeholder="Segundo Nombre*"
+              name="UO_segundo_nombre"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <input
+              className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
+              placeholder="Primer Apellido"
+              name="UO_primer_apellido"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <input
+              className="bg-white text-black text-center placeholder:text-[#D9D9D9] rounded mb-5"
+              placeholder="Segundo Apellido"
+              name="UO_segundo_apellido"
+              onChange={handleChange}
+            ></input>
+          </div>
+          <div
+            className="p-2
+                    lg:p-0"
+          >
+            <select className="bg-white text-[#D9D9D9] text-center placeholder:text-[#D9D9D9] rounded">
+              <option className=" text-[#D9D9D9] text-center" disabled>
+                Elige una ubicacion
+              </option>
+              <option
+                className=" text-[#D9D9D9] text-center"
+                value="Administrador"
+              >
+                Administrador
+              </option>
+              <option className="text-[#D9D9D9] text-center" value="Usuario">
+                Usuario
+              </option>
+            </select>
+          </div>
 
-                    <div className="p-2 mt-5">
-                        <button  className="bg-[#3726FD] p-2 text-white rounded">
-                            Añadir
-                        </button>
-                    </div>
-
-
-                </form>
-                <div className="bg-[#D9D9D9] rounded text-center items-center justify-center mx-10 p-2">
-                    <h1 className="text-white p-5">Lista de Administradores</h1>
-                    <div class="overflow-x-auto">
-                    <table class="table-fixed w-full mt-4">
-                        <thead class="text-sm md:text-2xl">
-                            <tr class="bg-white text-black md:pr-40 md:pl-40
+          <div className="p-2 mt-5">
+            <button className="bg-[#3726FD] p-2 text-white rounded">
+              Añadir
+            </button>
+          </div>
+        </form>
+        <div className="bg-[#D9D9D9] rounded text-center items-center justify-center mx-10 p-2">
+          <h1 className="text-white p-5">Lista de Administradores</h1>
+          <div class="overflow-x-auto">
+            <table class="table-fixed w-full mt-4">
+              <thead class="text-sm md:text-2xl">
+                <tr
+                  class="bg-white text-black md:pr-40 md:pl-40
                             lg:text-xs
-                            xl:text-2xl">
-                                <th class="p-4">Cédula</th>
-                                <th>Nombre</th>
-                                <th>Apellidos</th>
-                                <th>Rol</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr className="sm:text-sm sm:p-4
-                                " >
-                                <td className="
-                                    bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                
-                            </tr>
-                            <tr>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                
-                            </tr>
-                            <tr>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-
-                            </tr>
-                            <tr>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                                <td className="bg-[#D9D9D9] p-7">
-                                </td>
-                            </tr>
-                            <tr>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                                <td className="bg-[#989898] p-7">
-                                </td>
-                            </tr>
-                            
-                        </tbody>
-                    </table>
-                </div>
-                <div className="p-2">
-                        <button className="bg-[#3726FD] p-2 text-white rounded">
-                            Eliminar
-                        </button>
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+                            xl:text-2xl"
+                >
+                  <th class="p-4">Cédula</th>
+                  <th>Nombre</th>
+                  <th>Apellidos</th>
+                  <th>Rol</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr
+                  className="sm:text-sm sm:p-4
+                                "
+                >
+                  <td
+                    className="
+                                    bg-[#989898] p-7"
+                  ></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                  <td className="bg-[#D9D9D9] p-7"></td>
+                </tr>
+                <tr>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                  <td className="bg-[#989898] p-7"></td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+          <div className="p-2">
+            <button className="bg-[#3726FD] p-2 text-white rounded">
+              Eliminar
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
-
--- pauna/pages/api/config/admin.js
+++ pauna/pages/api/config/admin.js
@@ -4,14 +4,14 @@ export default async function handler(req, res) {
     console.log(req.method);
     switch (req.method) {
         case "GET":
-            return await getAll(req, res);
+            return await getAllAdministrador(req, res);
         case "POST":
             console.log(req.body);
             return await saveUserAdmin(req, res);
     }
 }
 
-const getAll = async (req, res) => {
+const getAllAdministrador = async (req, res) => {
     const [result] = await pool.query("SELECT * FROM `pau-gnl-rol`");
     console.log(result);
     return res.status(200).json(result);
@@ -19,13 +19,20 @@ const getAll = async (req, res) => {
 
 const saveUserAdmin = async (req, res) => {
     console.log(req.body);
-    const { RL_identificador, RL_nombre, RL_descripcion } = req.body;
+    const { UO_identificador,
+        UO_primer_nombre,
+        UO_segundo_nombre,
+        UO_primer_apellido,
+        UO_segundo_apellido } = req.body;
 
     const result = await pool
-        .query("INSERT INTO `pau-gnl-rol` SET ?", {
-            RL_identificador,
-            RL_nombre,
-            RL_descripcion,
+        .query("INSERT INTO `pau-gnl-usuario` SET ?", {
+            UO_identificador,
+            UO_primer_nombre,
+            UO_segundo_nombre,
+            UO_primer_apellido,
+            UO_segundo_apellido,
+            UO_identificador_rol: "R3"
         })
         .then(function (response) {
             console.log(response);

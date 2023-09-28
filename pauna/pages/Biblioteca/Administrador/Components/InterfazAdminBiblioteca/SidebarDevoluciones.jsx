7f82273445e4e841211724bf5d8afe595ce23fca
AndreyBV5 <127549686+AndreyBV5@users.noreply.github.com>
2023-09-28 10:40:56 -0600

    update

--- pauna/pages/Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarDevoluciones.jsx
+++ pauna/pages/Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarDevoluciones.jsx
@@ -260,7 +260,7 @@ const SidebarDevoluciones = () => {
                             </tr>
                         </tbody>
                     </table>
-                    <button className='rounded-xl text-white border-white mt-6 ml-3 w-40 h-12 bg-[#132335]'>Advertencia</button>
+                    <button className='rounded-xl text-white border-white mt-6 ml-3 w-40 h-12 bg-[#132335] hover:bg-[#242d66]'>Advertencia</button>
                 </div>
             </div>
     )

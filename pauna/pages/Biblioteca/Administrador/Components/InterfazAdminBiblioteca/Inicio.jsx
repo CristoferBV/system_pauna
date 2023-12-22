5a96b5d66bff41ef28d4fb129734913cb4428a70
AndreyBV5 <andrey0130player@gmail.com>
2023-12-22 14:54:36 -0600

    Nuevo Inicio BibliotecaAdmin

--- pauna/pages/Biblioteca/Administrador/Components/InterfazAdminBiblioteca/Inicio.jsx
+++ pauna/pages/Biblioteca/Administrador/Components/InterfazAdminBiblioteca/Inicio.jsx
@@ -1,12 +1,51 @@
-import React from 'react'
-import { Table } from 'react-bootstrap';
+import React from "react";
+import { Carousel, Card } from "react-bootstrap";
 
 const Inicio = () => {
   return (
-    <div>
-        
+    <div className="container">
+      <div className="row">
+        <div className="col-6">
+          <Card>
+            <Card.Body>
+              <Card.Title>Sistema Administrativo</Card.Title>
+              <Card.Text>
+                Bienvenido al modo administrativo.
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </div>
+        <div className="col-6">
+          <Carousel>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://img.freepik.com/foto-gratis/abundante-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29660.jpg"
+                alt="Primera imagen"
+                style={{ objectFit: "cover" }}
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://img.freepik.com/foto-gratis/abundante-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29660.jpg"
+                alt="Segunda imagen"
+                style={{ objectFit: "cover" }}
+              />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img
+                className="d-block w-100"
+                src="https://img.freepik.com/foto-gratis/abundante-coleccion-libros-antiguos-estantes-madera-generados-ia_188544-29660.jpg"
+                alt="Tercera imagen"
+                style={{ objectFit: "cover" }}
+              />
+            </Carousel.Item>
+          </Carousel>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;

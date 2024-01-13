b7ef6e834dfd78d9a1bff0c5cc420b5fec89c048
AndreyBV5 <andrey0130player@gmail.com>
2024-01-13 13:55:49 -0600

    limpiando info e import no funcional comentados

--- pauna/pages/index.js
+++ pauna/pages/index.js
@@ -1,18 +1,5 @@
 import { Inter } from 'next/font/google'
-import UserWindow from './Administracion/Components/User/userWindow'
-import HomeClient from './Biblioteca/Cliente/Components/InterfazCliente/HomeClient'
-import LoanClient from './Biblioteca/Cliente/Components/InterfazCliente/LoanClient'
-import DevolutionClient from './Biblioteca/Cliente/Components/InterfazCliente/DevolutionClient'
 import Login from './LoginAndRegister/Login/Login'
-import Register from './LoginAndRegister/Register/Register'
-
-import Slidebar from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/Slidebar'
-import SidebarCitas from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarCitas'
-import SidebarDevoluciones from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarDevoluciones'
-import SidebarEstudiantes from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarEstudiantes'
-import SidebarReporte from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/SidebarReporte'
-import Sidebar from './Administracion/Components/Sidebar/Sidebar'
-import NavbarAdminBiblioteca from './Biblioteca/Administrador/Components/InterfazAdminBiblioteca/NavbarAdminBiblioteca'
 
 
 const inter = Inter({ subsets: ['latin'] })
@@ -20,16 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <>
-      {/* <UserWindow/> */}
-      {/* <Slidebar/> */}
-      {/* <HomeClient/> */}
-      {/* <LoanClient/> */}
-      {/* <DevolutionClient/> */}
-      {/* <ProfileClient/> */}
-      {/* <PaginaCirculo/> */}
       <Login/>
-      {/* <Sidebar/> */}
-      {/* <NavbarAdminBiblioteca/> */}
     </>
 
   )
